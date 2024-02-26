1c367859fd936419022ce59adb218518495f7f8d
Tom C <tom.carlier@epitech.eu>
2024-02-26 14:22:25 +0100

    [build] add the Makefile, the include and the src with the main.c

---
+++ include/my.h
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#pragma once
+    #define MY_H
+    #define  _GNU_SOURCE
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <time.h>
+    #include <limits.h>
+    #include <stdarg.h>
+    #include <errno.h>
+    #include <fcntl.h>
+    #include <math.h>
+    #include <stdlib.h>
+    #include <sys/stat.h>
+    #include <stdbool.h>
+
+    #include <SFML/Graphics.h>
+    #include <SFML/Audio.h>
+    #include <SFML/Config.h>
+    #include <SFML/GPUPreference.h>
+    #include <SFML/Network.h>
+    #include <SFML/OpenGL.h>
+    #include <SFML/System.h>
+    #include <SFML/Window.h>
