4418ce6e00b2dbb394acac724f55dc2db12d7c19
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-07 13:20:37 +0100

    [refactor] -->  add the prototype for the function 'display_help'

--- include/my.h
+++ include/my.h
@@ -69,3 +69,5 @@ int zone_draw(zone_t *, window_t *);
 int zone_remove(zone_t **, char const *);
 int zone_sort(zone_t **);
 int zone_reverse(zone_t **);
+
+int display_help(int, char **, char const *);
