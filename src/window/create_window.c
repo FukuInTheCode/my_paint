4cb795a745757b66e8241887219a58b74db98706
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-11 10:34:11 +0100

    [feat] -->  add the fuction for the event for close the window

--- src/window/create_window.c
+++ src/window/create_window.c
@@ -7,10 +7,22 @@
 
 #include "my.h"
 
-int create_window(window_t *window)
+int gestion_event(window_t *window, sfEvent *event)
+{
+    while (sfRenderWindow_pollEvent(window->window, event)) {
+        if (event->type == sfEvtClosed)
+            sfRenderWindow_close(window->window);
+        if (event->type == sfEvtKeyPressed && event->key.code == sfKeyEscape)
+            sfRenderWindow_close(window->window);
+    }
+    return 0;
+}
+
+int create_window(window_t *window, sfEvent *event)
 {
     sfRenderWindow_setFramerateLimit(window->window, 60);
     while (sfRenderWindow_isOpen(window->window)) {
+        gestion_event(window, event);
         gestion_window(window);
     }
     return 0;
@@ -19,10 +31,12 @@ int create_window(window_t *window)
 int my_gimp(void)
 {
     window_t window;
+    sfEvent event;
 
     if (init_struct_window(&window) == 84)
         return 84;
-    if (create_window(&window) == 84)
+    if (create_window(&window, &event) == 84)
         return 84;
+    megatron_free(&window);
     return 0;
 }
