315f1a199d8116bd99f4705b7a698a8531e67b2b
Tom C <tom.carlier@epitech.eu>
2024-03-11 23:16:37 +0100

    [feat] -->  add the zone_get_mouse()

--- include/my.h
+++ include/my.h
@@ -79,3 +79,4 @@ int zone_reverse(zone_t **);
 int display_help(int, char **, char const *);
 zone_t *zone_create(void);
 int zone_destroy(zone_t *);
+zone_t *zone_get_mouse(zone_t *, window_t *, sfVector2f);
