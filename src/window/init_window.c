6f3febff938b74e901b03758aeec5a4983411e0f
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-11 10:10:16 +0100

    [feat] -->  add function to initialize the window structure

---
+++ src/window/init_window.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** init_window.c
+** File description:
+** init the struct for the window
+*/
+
+#include "my.h"
+
+int init_struct_window(window_t *window)
+{
+    window->window = sfRenderWindow_create((sfVideoMode){1920, 1080, 32}, "My Gimp :)", sfFullscreen, NULL);
+    if(!window->window)
+        return 84;
+    return 0;
+}
