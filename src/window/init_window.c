81d0d57a641d06663af1facdfecc65bb2f710488
Tom C <tom.carlier@epitech.eu>
2024-03-12 13:18:09 +0100

    [fix] --> remove the debug printf

--- src/window/init_window.c
+++ src/window/init_window.c
@@ -18,7 +18,5 @@ int init_struct_window(window_t *window)
     zone_set_size(window->head, window, (sfVector2f){1920, 1080});
     canva_add_layer(window->head->extra_information, layer_create());
     zone_add(&window->head, burgerzone_create());
-    for (zone_t *tmp = window->head; tmp; tmp = tmp->next)
-        printf("%s\n", tmp->name);
     return 0;
 }
