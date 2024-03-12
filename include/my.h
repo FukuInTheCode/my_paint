eb5a94928e98628930407827456818d8d1791875
Tom C <tom.carlier@epitech.eu>
2024-03-12 11:39:37 +0100

    [fix] --> remove the crash

--- include/my.h
+++ include/my.h
@@ -95,6 +95,7 @@ int zone_reverse(zone_t **);
 int display_help(int, char **, char const *);
 zone_t *zone_create(void);
 int zone_destroy(zone_t *);
+int zone_set_size(zone_t *, window_t *, sfVector2f);
 
 zone_t *zone_get_mouse(zone_t *, window_t *, sfVector2f);
 
