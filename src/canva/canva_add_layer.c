4f17ef18c2d681d8d6c4bf8803a1b0279d480e58
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-12 09:35:18 +0100

    [fix] --> fix the warning during compilation

--- src/canva/canva_add_layer.c
+++ src/canva/canva_add_layer.c
@@ -11,7 +11,7 @@ int canva_add_layer(canva_t *canva, layer_t *layer)
 {
     if (!canva->main)
         canva->main = layer;
-    layer_add(canva->layers, layer);
+    layer_add(&canva->layers, layer);
     return 0;
 
 }
