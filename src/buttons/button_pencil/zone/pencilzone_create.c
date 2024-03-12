8a1802109f6a0978ac23eb3a6add92ad3a4b7c9c
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-13 00:04:30 +0100

    [feat] -->  add the function for create the zone of the pencil

---
+++ src/buttons/button_pencil/zone/pencilzone_create.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** pencilzone_create.c
+** File description:
+** pencilzone_create.c
+*/
+
+#include "my.h"
+
+zone_t *pencilzone_create(window_t *window)
+{
+    zone_t *zone = zone_create();
+    pencil_t *pencil = pencil_create();
+
+    zone->extra_information = (void *)pencil;
+    zone->name = my_strdup("pencil");
+    zone->depend_corner = UPPER_RIGHT;
+    zone->depend_on = zone_get(window->head, "burger");
+    zone->priority = 1;
+    zone->draw_f = pencilzone_draw;
+    zone->size.x = 40;
+    zone->size.y = 40;
+    return zone;
+}
