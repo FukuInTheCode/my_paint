9e72587919b0652cafe7094710957705dfe3a2e8
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-13 13:22:12 +0100

    [refactor] -->  add the adjust

--- src/buttons/button_pencil/zone/pencilzone_create.c
+++ src/buttons/button_pencil/zone/pencilzone_create.c
@@ -16,9 +16,10 @@ zone_t *pencilzone_create(window_t *window)
     zone->name = my_strdup("button_pencil");
     zone->depend_corner = UPPER_RIGHT;
     zone->depend_on = zone_get(window->head, "burger");
-    zone->priority = 1;
+    zone->priority = 2;
     zone->draw_f = pencilzone_draw;
     zone->size.x = 40;
     zone->size.y = 40;
+    zone->adjust = (sfVector2f){20, 30};
     return zone;
 }
