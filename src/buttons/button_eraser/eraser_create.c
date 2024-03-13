260b4e8b9f63dc54c190e8924eed9089caac6700
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-13 13:26:53 +0100

    [feat] -->  add the function for create the eraser button

---
+++ src/buttons/button_eraser/eraser_create.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** eraser_create.c
+** File description:
+** eraser_create.c
+*/
+
+#include "my.h"
+
+eraser_t *eraser_create(void)
+{
+    eraser_t *eraser = malloc(sizeof(eraser_t));
+
+    eraser->is_clicked = false;
+    eraser->rect = sfRectangleShape_create();
+    eraser->sprite = sfSprite_create();
+    sfRectangleShape_setSize(eraser->rect, (sfVector2f){40, 40});
+    sfRectangleShape_setFillColor(eraser->rect, sfWhite);
+    sfRectangleShape_setOutlineColor(eraser->rect, sfBlack);
+    sfRectangleShape_setOutlineThickness(eraser->rect, 2);
+    sfTexture *texture = sfTexture_createFromFile("src/assets/eraser.jpg", NULL);
+    sfSprite_setTexture(eraser->sprite, texture, sfTrue);
+    sfVector2f scale = {0.13f, 0.13f};
+    sfSprite_setScale(eraser->sprite, scale);
+    return eraser;
+}
