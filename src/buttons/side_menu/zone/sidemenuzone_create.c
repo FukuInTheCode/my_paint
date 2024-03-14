5d396410ff0363f197bd81e89ad80bc5a99c73d3
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-14 15:04:02 +0100

    [feat] -->  add the function for add and draw the zone of the side menu

---
+++ src/buttons/side_menu/zone/sidemenuzone_create.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** sidemenuzone_create.c
+** File description:
+** sidemenuzone_create.c
+*/
+
+#include "my.h"
+
+zone_t *sidemenuzone_create(window_t *window)
+{
+    zone_t *zone = zone_create();
+    shape_t *sidemenu = sidemenu_create();
+
+    zone->extra_information = (void *)sidemenu;
+    zone->name = my_strdup("button_sidemenu");
+    zone->depend_corner = LOWER_LEFT;
+    zone->depend_on = zone_get(window->head, "burger");
+    zone->priority = 2;
+    zone->draw_f = sidemenuzone_draw;
+    zone->size.x = 100;
+    zone->size.y = 40;
+    return zone;
+}
